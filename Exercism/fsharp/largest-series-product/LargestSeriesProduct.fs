51779908eda0073a17c71f43694fc87d74d17568
Tim <TimCodesStuff@hotmail.com>
2019-07-08 05:25:20 -0700

    Update LargestSeriesProduct.fs
    Just doing a little cleanup.
    - Using Seq.Reduce instead of Seq.Fold
    - Cleaning up "None" conditions
    - Doing .ToString() and int cast in same line

--- Exercism/fsharp/largest-series-product/LargestSeriesProduct.fs
+++ Exercism/fsharp/largest-series-product/LargestSeriesProduct.fs
@@ -5,23 +5,21 @@ open System.Text.RegularExpressions
 let largestProduct (input : string) seriesLength : int option = 
     let rec findLargestProductOfLengthX x largest (input : int list) = 
         if input.Length >= x then
-            let firstX = input |> Seq.take x
-            let localProd = firstX |> Seq.fold (fun acc a -> acc * a) 1
+            let localProd = input |> Seq.take x |> Seq.reduce (*)
             let newLargsest = if largest > localProd then largest else localProd
             findLargestProductOfLengthX x newLargsest input.Tail
         else
-            largest
+            largest   
 
     if seriesLength = 0 then
         Some 1
-    else if not ((new Regex(@"^[0-9]+$")).IsMatch input) then
-        None
-    else if seriesLength < 0 || input.Length < seriesLength then
+    elif not ((new Regex(@"^[0-9]+$")).IsMatch input) 
+            || seriesLength < 0 
+            || input.Length < seriesLength then
         None
     else
         input
         |> List.ofSeq
-        |> List.map (fun x -> x.ToString())
-        |> List.map (fun x -> x |> int)
+        |> List.map (fun x -> x.ToString() |> int)
         |> findLargestProductOfLengthX seriesLength 0
         |> Some
