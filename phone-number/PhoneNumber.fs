f45ed5c22eb48455904b94797233bb227f9097fa
Tim <TimCodesStuff@hotmail.com>
2019-08-17 07:07:21 -0700

    A little more cleanup on phone numbers.
    Found a case where I was checking for the same condition twice, went ahead and removed that.

--- phone-number/PhoneNumber.fs
+++ phone-number/PhoneNumber.fs
@@ -13,25 +13,21 @@ let clean (input: string) =
         |> Seq.filter (fun x -> Char.IsDigit(x))
         |> String.Concat
         |> fun cleanedInput ->    
-            match cleanedInput.Length with
-            | 11 -> if cleanedInput.[0] = '1' then 
-                        match cleanedInput.[1] with
-                        | '0' -> Error "area code cannot start with zero"
-                        | '1' -> Error "area code cannot start with one"
-                        | _ -> match cleanedInput.[4] with
-                            | '0' -> Error "exchange code cannot start with zero"
-                            | '1' -> Error "exchange code cannot start with one"
-                            |_ -> Ok (uint64(cleanedInput.Substring(1)))
-                    else Error "11 digits must start with 1"
-            | 10 -> if cleanedInput.Length = 10 then 
-                        match cleanedInput.[0] with
-                        | '0' -> Error "area code cannot start with zero"
-                        | '1' -> Error "area code cannot start with one"
-                        | _ -> match cleanedInput.[3] with
-                            | '0' -> Error "exchange code cannot start with zero"
-                            | '1' -> Error "exchange code cannot start with one"
-                            |_ -> Ok (uint64(cleanedInput)) 
-                    else Error "Numbers starting with a non '1' should be 10 digits long."
-            | x when x > 11 -> Error "more than 11 digits"
-            | _ -> Error "incorrect number of digits"
+            match (cleanedInput.Length, cleanedInput.[0]) with
+            | 11, '1' -> match cleanedInput.[1] with
+                         | '0' -> Error "area code cannot start with zero"
+                         | '1' -> Error "area code cannot start with one"
+                         | _ -> match cleanedInput.[4] with
+                                | '0' -> Error "exchange code cannot start with zero"
+                                | '1' -> Error "exchange code cannot start with one"
+                                | _ -> Ok (uint64(cleanedInput.Substring(1)))
+            | 11, _ -> Error "11 digits must start with 1"
+            | 10, '0' -> Error "area code cannot start with zero"
+            | 10, '1' -> Error "area code cannot start with one"
+            | 10, _ -> match cleanedInput.[3] with
+                       | '0' -> Error "exchange code cannot start with zero"
+                       | '1' -> Error "exchange code cannot start with one"
+                       | _ -> Ok (uint64(cleanedInput))
+            | x, _ when x > 11 -> Error "more than 11 digits"
+            | _, _ -> Error "incorrect number of digits"
            
