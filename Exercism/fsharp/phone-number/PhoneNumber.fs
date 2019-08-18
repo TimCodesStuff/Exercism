1197eb66c36330db45c8fd063314f69885759a33
Tim <TimCodesStuff@hotmail.com>
2019-08-18 12:24:54 -0700

    Completed PhoneNumber by using the Result monad.
    Added a result monad, and used it to unwrap a series of functions that validate a phone number.
    
    This is much cleaner and more readable than the previous implementation, and also removes duplicatino I had for checking the exchange code.
    
    Also updated to the latest, they had changed the tests to look for "letters" instead of "alphanumerics".

--- Exercism/fsharp/phone-number/PhoneNumber.fs
+++ Exercism/fsharp/phone-number/PhoneNumber.fs
@@ -1,33 +1,67 @@
 ﻿module PhoneNumber
 
-open System
+open Result
+open System 
 
-let clean (input: string) =   
-    let allowedPunctuation = Set.ofList [' '; '-'; '('; ')'; '+'; '.']
+// Ensures no letter.
+let CleanLetters (input : seq<char>) =
+    input 
+    |> Seq.exists(Char.IsLetter) 
+    |> function
+        | true -> Error "letters not permitted"
+        | false -> Ok (input |> Seq.filter(Char.IsDigit))
+
+// Removes the first char if it's a plus sign.
+let CleanBeginningPlus (input : seq<char>) =
+    if (Seq.head input) = '+' then Seq.tail input else input
+
+// Removes allowed punctuation, then ensures no punctuation left.
+let CleanPunctuation (input : seq<char>) =
+    let allowedPunctuation = Set.ofList [' '; '-'; '('; ')'; '.']
+    let cleaned = 
+        input 
+        |> CleanBeginningPlus
+        |> Seq.filter(fun c -> not (allowedPunctuation.Contains c))
     
-    match input |> Seq.filter(fun x -> not (allowedPunctuation.Contains x)) with
-    | phoneNumber when phoneNumber |> Seq.exists(fun x -> Char.IsLetter(x)) ->  Error "alphanumerics not permitted"
-    | phoneNumber when phoneNumber |> Seq.exists(fun x -> not (Char.IsLetterOrDigit(x))) ->  Error "punctuations not permitted"
-    | phoneNumber -> 
-        phoneNumber
-        |> Seq.filter (fun x -> Char.IsDigit(x))
-        |> String.Concat
-        |> fun cleanedInput ->    
-            match (cleanedInput.Length, cleanedInput.[0]) with
-            | 11, '1' -> match cleanedInput.[1] with
-                         | '0' -> Error "area code cannot start with zero"
-                         | '1' -> Error "area code cannot start with one"
-                         | _ -> match cleanedInput.[4] with
-                                | '0' -> Error "exchange code cannot start with zero"
-                                | '1' -> Error "exchange code cannot start with one"
-                                | _ -> Ok (uint64(cleanedInput.Substring(1)))
-            | 11, _ -> Error "11 digits must start with 1"
-            | 10, '0' -> Error "area code cannot start with zero"
-            | 10, '1' -> Error "area code cannot start with one"
-            | 10, _ -> match cleanedInput.[3] with
-                       | '0' -> Error "exchange code cannot start with zero"
-                       | '1' -> Error "exchange code cannot start with one"
-                       | _ -> Ok (uint64(cleanedInput))
-            | x, _ when x > 11 -> Error "more than 11 digits"
-            | _, _ -> Error "incorrect number of digits"
-           
+    cleaned
+    |> Seq.exists(Char.IsPunctuation)
+    |> function
+       | true -> Error "punctuations not permitted"
+       | false -> Ok cleaned
+       
+// Ensures that a ten digit number begins correctly.
+let ValidateTenDigitNumber (input : seq<char>) =
+    match Seq.head input with
+    | '0' -> Error "area code cannot start with zero"
+    | '1' -> Error "area code cannot start with one"
+    | _ -> Ok input
+
+// Ensures that an eleven digit number is structures correctly.
+let ValidateElevenDigitNumber (input : seq<char>) =
+    if Seq.head input = '1' then 
+        ValidateTenDigitNumber (Seq.tail input)
+        else Error "11 digits must start with 1" 
+
+// Ensures a 10 or 11 digit number is structured correctly.
+let ValidateNumber (input : seq<char>) =
+    match Seq.length input with
+    | 11 -> ValidateElevenDigitNumber input
+    | 10 -> ValidateTenDigitNumber input
+    | x when x > 11 -> Error "more than 11 digits"
+    | _ -> Error "incorrect number of digits"
+
+// Ensures a 10 digit code is structured correctly.
+let ValidateExchangeCodeOnTenDigitNumber (input : seq<char>) =
+    let phoneNumber = String.Concat(input)
+    match phoneNumber.[3] with
+    | '0' -> Error "exchange code cannot start with zero"
+    | '1' -> Error "exchange code cannot start with one"
+    | _ -> Ok (uint64(phoneNumber))
+
+let clean (input: string) =
+    result { 
+        let! noPunctNumber = CleanPunctuation input
+        let! cleanPhoneNumber = CleanLetters noPunctNumber 
+        let! cleanTenDigitNumber = ValidateNumber cleanPhoneNumber
+        return! ValidateExchangeCodeOnTenDigitNumber cleanTenDigitNumber
+        }  
