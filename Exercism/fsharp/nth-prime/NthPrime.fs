c1f047202facb75b4e9253309751147ef002008d
Tim <TimCodesStuff@hotmail.com>
2019-08-19 22:07:59 -0700

    Initial submission of the Nth prime problem.
    Just a quick solution, I can clean it up more but it passes the test at least. Next time baby is asleep again, I'll take another crack at it.

---
+++ Exercism/fsharp/nth-prime/NthPrime.fs
@@ -0,0 +1,13 @@
+﻿module NthPrime
+
+let prime nth : int option =
+    let isprime n =
+        let rec check i =
+            i > n/2 || (n % i <> 0 && check (i + 1))
+        check 2
+
+    if nth < 1 then 
+        None
+    else
+        let cieling = if nth < 6 then 13 else (nth * nth)
+        Some (Seq.item nth (seq {for n in 1..cieling do if isprime n then yield n}))
