6d8b9664c2a710fbe436b17a5175d2c87a9fd302
Tim <TimCodesStuff@hotmail.com>
2019-08-21 00:55:55 -0700

    Update bob.fs
    Making the IsSilence function a little more clear.

--- Exercism/fsharp/bob/Bob.fs
+++ Exercism/fsharp/bob/Bob.fs
@@ -5,8 +5,8 @@ open System
 let IsSilence (input: string): bool =
     input.Length = 0 ||
     input
-    |> Seq.exists (fun c -> not (Char.IsWhiteSpace c))
-    |> not
+    |> Seq.filter (fun c -> not (Char.IsWhiteSpace c))
+    |> Seq.isEmpty
 
 let IsYell (input: string): bool =
     if (Seq.isEmpty (Seq.filter (Char.IsLetter) input)) then
@@ -25,7 +25,7 @@ let IsYelledQuestion (input: string): bool =
 
 let response (input: string): string = 
     match input.TrimEnd() with
-    | str when IsSilence str-> "Fine. Be that way!"
+    | str when IsSilence str -> "Fine. Be that way!"
     | str when IsYelledQuestion str -> "Calm down, I know what I'm doing!"
     | str when IsQuestion str -> "Sure."
     | str when IsYell str -> "Whoa, chill out!"
