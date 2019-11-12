a79c35dc521899a8b3c2a198e99d53ab8f0aa34d
Tim <TimCodesStuff@hotmail.com>
2019-11-12 15:39:44 -0800

    Initial attempt at Binary Search Tree
    Implemented this using mutable values. It's easier to read this way I think, but I'll take a stab at cleaning it up since the tests technically always pass in the full set of data when you create the BST.

---
+++ Exercism/fsharp/binary-search-tree/BinarySearchTree.fs
@@ -0,0 +1,41 @@
+﻿module BinarySearchTree
+
+open Node
+
+let left node  = node.left
+let right node = node.right
+let data node = node.data
+
+let rec AddNodeToTree (treeRoot : Node) (nodeToAdd : Node) =
+    if (data treeRoot) >= (data nodeToAdd) then
+        match left treeRoot with 
+        | Some leftNode -> AddNodeToTree leftNode nodeToAdd |> ignore
+        | None -> treeRoot.left <- Some nodeToAdd
+    else
+        match right treeRoot with 
+        | Some rightNode -> AddNodeToTree rightNode nodeToAdd |> ignore
+        | None -> treeRoot.right <- Some nodeToAdd
+
+    treeRoot
+
+let create (items : int seq) =
+    let rootNode = CreateNode (Seq.head items)
+    items 
+    |> Seq.tail
+    |> Seq.fold (fun root newData -> AddNodeToTree root (CreateNode newData)) rootNode
+
+let sortedData node =
+    let rec getData (rootNode: Node) (sortedDataList : int list) = 
+        let listWithLeftData = 
+            match left rootNode with
+            | Some leftNode -> getData leftNode sortedDataList
+            | None -> sortedDataList
+
+        let listWithNodeData = 
+            List.append listWithLeftData [rootNode.data]
+
+        match right rootNode with 
+        | Some rightNode -> getData rightNode listWithNodeData
+        | None -> listWithNodeData
+
+    getData node []
---
+++ Exercism/fsharp/binary-search-tree/Node.fs
@@ -0,0 +1,14 @@
+﻿module Node
+
+type Node = {
+    data : int;
+    mutable left: Node option;
+    mutable right: Node option;
+    }
+
+let CreateNode (rootData : int) =
+    {
+        data = rootData;
+        left = None;
+        right = None;
+    }
