ae59367c1706883e08c38697e17fbd3bdaae62ac
Tim <TimCodesStuff@hotmail.com>
2019-11-20 10:54:20 -0800

    Submitting initial solution for pangram.
    This one is pretty straight foward, I might be able to make a few optimizations but I'm just gonna move on.

---
+++ Exercism/csharp/pangram/Pangram.cs
@@ -0,0 +1,14 @@
+﻿public static class Pangram
+{
+    public static bool IsPangram(string input)
+    {
+        foreach (char c in "abcdefghijklmnopqrstuvwxyz")
+        {
+            if (!input.ToLower().Contains(c))
+            {
+                return false;
+            }
+        }
+        return true;
+    }
+}
