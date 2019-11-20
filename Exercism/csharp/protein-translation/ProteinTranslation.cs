bdfca934aeca5b31df04b6ba4cafe44375001ae4
Tim <TimCodesStuff@hotmail.com>
2019-11-20 12:11:00 -0800

    Adding initial implementation for ProteinTranslation.
    Could be optimized so that I stop translating if I find a "STOP" protein, but this works for now.

---
+++ Exercism/csharp/protein-translation/ProteinTranslation.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Linq;
+using System.Collections.Generic;
+
+public static class ProteinTranslation
+{
+    public static Dictionary<string, string> ProteinDictionary = new Dictionary<string, string>()
+    {
+        { "AUG","Methionine" },
+        { "UUU","Phenylalanine" },
+        { "UUC","Phenylalanine" },
+        { "UUA","Leucine" },
+        { "UUG","Leucine" },
+        { "UCU","Serine" },
+        { "UCC","Serine" },
+        { "UCA","Serine" },
+        { "UCG","Serine" },
+        { "UAU","Tyrosine" },
+        { "UAC","Tyrosine" },
+        { "UGU","Cysteine" },
+        { "UGC","Cysteine" },
+        { "UGG","Tryptophan" },
+        { "UAA","STOP" },
+        { "UAG","STOP" },
+        { "UGA","STOP" },
+    };
+
+    public static IEnumerable<string> ChunkBySize(string input, int chunkSize)
+    {
+        return Enumerable.Range(0, input.Length / chunkSize)
+               .Select(i => input.Substring(i * chunkSize, chunkSize));
+    }
+
+    public static string[] Proteins(string strand)
+    {
+        return ChunkBySize(strand, 3)
+               .Select(codon => ProteinDictionary.GetValueOrDefault(codon))
+               .TakeWhile(protein => protein != "STOP").ToArray();
+    }
+}
