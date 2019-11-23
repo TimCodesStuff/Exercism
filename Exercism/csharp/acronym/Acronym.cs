d2f86044f70608385978c3097af9071d5afe3bdc
Tim <TimCodesStuff@hotmail.com>
2019-11-23 10:44:11 -0800

    Submitting solution for Acronym.

---
+++ Exercism/csharp/acronym/Acronym.cs
@@ -0,0 +1,9 @@
+﻿using System;
+using System.Linq;
+
+public static class Acronym
+{
+    public static string Abbreviate(string phrase) => phrase.Split(new char[] { ' ', '-', '_' }, StringSplitOptions.RemoveEmptyEntries)
+                                                            .Select(word => word.Trim().ToUpper()[0])
+                                                            .Aggregate("", (acronym, newLetter) => acronym + newLetter);
+}
