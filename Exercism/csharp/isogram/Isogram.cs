8ca7bfd6960876e2f6363abb938923843fdbcf0b
Tim <TimCodesStuff@hotmail.com>
2019-11-23 08:12:44 -0800

    Submitting solution for Isogram.

---
+++ Exercism/csharp/isogram/Isogram.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+
+public static class Isogram
+{
+    public static bool IsIsogram(string word)
+    {
+        IEnumerable<char> lowercaseWord = word.ToLower().Where(char.IsLetter);
+        return lowercaseWord.Count() == lowercaseWord.Distinct().Count();
+    }
+}
