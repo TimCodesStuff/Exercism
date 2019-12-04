60c0c1398263bd80fe3e475d1a164ac79551f5fa
Tim <TimCodesStuff@hotmail.com>
2019-12-04 11:41:13 -0800

    Solution for Resistor Color.

---
+++ Exercism/csharp/resistor-color/ResistorColor.cs
@@ -0,0 +1,10 @@
+﻿using System;
+
+public static class ResistorColor
+{
+    private static readonly string[] colors = new[] { "black", "brown", "red", "orange", "yellow", "green", "blue", "violet", "grey", "white" };
+
+    public static int ColorCode(string color) => Array.FindIndex(colors, x => x == color);
+
+    public static string[] Colors() => colors;
+}
